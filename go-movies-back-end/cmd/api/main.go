4999c6efac9bb61d2f44bdd1a0ecd1a47bd63221
ono5 <kuroneko_head_lord@yahoo.co.jp>
2024-03-18 06:16:07 +0900

    add backend

---
+++ go-movies-back-end/cmd/api/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"backend/internal/repository"
+	"backend/internal/repository/dbrepo"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+	DSN    string
+	Domain string
+	DB     repository.DatabaseRepo
+}
+
+func main() {
+	// set application configuration
+	var app application
+
+	// read from command line arguments
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=postgres sslmode=disable timezone=UTC connect_timeout=5", "Postgres connectoin string")
+	flag.Parse()
+
+	// connect to the database
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
+	defer app.DB.Connection().Close()
+
+	app.Domain = "example.com"
+	log.Println("Starting server on port", port, "with domain", app.Domain)
+
+	// start a web server
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go-movies-back-end/internal/repository/dbrepo/postgres_dbrepo.go
@@ -0,0 +1,52 @@
+package dbrepo
+
+import (
+	"backend/internal/models"
+	"context"
+	"database/sql"
+	"time"
+)
+
+type PostgresDBRepo struct {
+	DB *sql.DB
+}
+
+const dbTimeout = time.Second * 3
+
+func (m *PostgresDBRepo) Connection() *sql.DB {
+	return m.DB
+}
+
+func (m *PostgresDBRepo) AllMovies() ([]*models.Movie, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `SELECT id, title, release_date, runtime, mpaa_rating, description, coalesce(image, ''), created_at, updated_at FROM movies order by title;`
+	var movies []*models.Movie
+
+	rows, err := m.DB.QueryContext(ctx, query)
+	if err != nil {
+		return movies, err
+	}
+
+	for rows.Next() {
+		var movie models.Movie
+		err = rows.Scan(
+			&movie.ID,
+			&movie.Title,
+			&movie.ReleaseDate,
+			&movie.RunTime,
+			&movie.MPAARating,
+			&movie.Description,
+			&movie.Image,
+			&movie.CreatedAt,
+			&movie.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		movies = append(movies, &movie)
+	}
+
+	return movies, nil
+}
